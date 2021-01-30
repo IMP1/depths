2a756802a5de43e5fc04e194fd837ae16d5eb372
IMP1 <huw_taylor@hotmail.co.uk>
2021-01-30 13:19:36 +0000

    Remove unnecessary empty functions

--- scn/_base.lua
+++ scn/_base.lua
@@ -12,40 +12,15 @@ function scene.new(name)
 end
 
 function scene:load()
-    
-end
-
-function scene:keyPressed(key, isRepeat)
-
-end
-
-
-function scene:keyReleased(key, isRepeat)
-
-end
-
-function scene:keyTyped(text)
-
-end
-
-function scene:mousePressed(mx, my, key)
-
-end
-
-function scene:mouseReleased(mx, my, key)
-
 end
 
 function scene:update(dt, mx, my)
-
 end
 
 function scene:draw()
-
 end
 
 function scene:close()
-
 end
 
 return scene
