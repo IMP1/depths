7d3e0f4ae99e09c3b71fa060fad83632a3a3fa4b
IMP1 <huw_taylor@hotmail.co.uk>
2021-01-31 13:52:22 +0000

    Add framework for level gameplay

---
+++ lib/screenshake.lua
@@ -0,0 +1,47 @@
+local screenshake = {}
+
+local shakes = {}
+local rumble_x, rumble_y = 0, 0
+local total_shake_x, total_shake_y = 0, 0
+
+function screenshake.add_screenshake(duration, x, y)
+    table.insert(shakes, {
+        duration = duration,
+        timer = 0,
+        strength_x = x,
+        strength_y = (y or x),
+    })
+end
+
+function screenshake.add_rumble(x, y)
+    rumble_x = rumble_x + x
+    rumble_y = rumble_y + (y or x)
+end
+
+function screenshake.update(dt)
+    total_shake_x = rumble_x
+    total_shake_y = rumble_y
+    for i = #shakes, 1, -1 do
+        local shake = shakes[i]
+        shake.timer = shake.timer + dt
+        if shake.timer >= shake.duration then
+            table.remove(shakes, i)
+        else
+            total_shake_x = total_shake_x + shake.strength_x
+            total_shake_y = total_shake_y + shake.strength_y
+        end
+    end
+end
+
+function screenshake.set()
+    love.graphics.push()
+    local x = math.random() * total_shake_x * 2 - total_shake_x
+    local y = math.random() * total_shake_y * 2 - total_shake_y
+    love.graphics.translate(x, y)
+end
+
+function screenshake.unset()
+    love.graphics.pop()
+end
+
+return screenshake
--- scn/level.lua
+++ scn/level.lua
@@ -1,5 +1,6 @@
 local scene_manager = require 'lib.conductor'
 local camera        = require 'lib.camera'
+local screenshake   = require 'lib.screenshake'
 local level         = require 'cls.level.level'
 local base_scene    = require 'scn._base'
 
@@ -23,11 +24,38 @@ function scene.new(party, depth)
         depth      = depth or 1,
         seed       = os.time(),
     })
+    self.projectiles = {}
+    self.popups = {}
+    self.item_drops = {}
+    self.visited = {}
+    self.visible = {}
+    self.exiting_map = false
+    self.animations = {}
+    self.traps = {}
+    self.enemies = {}
+    -- TODO: Push gate graphic animation to close behind the players to animations
+    self.players = {}
+    for _, p in pairs(party) do
+        -- table.insert(self.players, )
+        -- TODO: Create a new player character for each player
+    end
 
     return self
 end
 
+function scene:load()
+    love.graphics.setBackgroundColor(0.25, 0.25, 0.3)
+end
+
 function scene:update(dt)
+    if self.map then
+        self:update_game(dt)
+    else
+        self:update_level_generation()
+    end
+end
+
+function scene:update_level_generation()
     local status = love.thread.getChannel("level-gen-status"):pop()
     if status then 
         self.map_generator_status = status
@@ -37,6 +65,54 @@ function scene:update(dt)
         self.map = level.new(map_result)
         self.map_generator:release()
         self.map_generator = nil
+        self.visited = {}
+        self.visible = {}
+        for j, row in pairs(self.map.tiles) do
+            self.visited[j] = {}
+            self.visible[j] = {}
+            for i, t in pairs(row) do
+                self.visited[j][i] = false
+                self.visible[j][i] = false
+            end
+        end
+        self.enemies = self.map.enemies
+        self.traps = self.map.traps
+        self:update_game(0)
+    end
+end
+
+function scene:update_game(dt)
+    self:update_animations(dt)
+    self:update_players(dt)
+    -- self:update_enemies(dt)
+    -- self:update_camera(dt)
+    -- self:update_traps(dt)
+    -- self:update_projectiles(dt)
+    -- self:update_item_drops(dt)
+    -- self:update_popups(dt)
+    -- self:remove_dead()
+    screenshake.update(dt)
+end
+
+function scene:update_animations(dt)
+    for i = #self.animations, 1, -1 do
+        local animation = self.animations[i]
+        animation:update(dt)
+        if animation.finished then
+            table.remove(self.animations, i)
+        end
+    end
+end
+
+function scene:update_players(dt)
+    for j, row in pairs(self.map.tiles) do
+        for i, t in pairs(row) do
+            self.visible[j][i] = false
+        end
+    end
+    for _, p in pairs(self.players) do
+        -- p:update(dt)
+        -- p:update_visibility()
     end
 end
 
@@ -44,6 +120,68 @@ function scene:draw()
     if not self.map then
         love.graphics.setColor(1, 1, 1)
         love.graphics.printf(self.map_generator_status, 0, 64, love.graphics.getWidth(), "center")
+    else
+        self:draw_game()
+    end
+end
+
+function scene:draw_game()
+    self.camera:set()
+    screenshake.set()
+    self:draw_map()
+    screenshake.unset()
+    self.camera:unset()
+    self:draw_minimap()
+end
+
+function scene:draw_map()
+    local tile_size = self.map.TILE_SIZE
+    for j, row in pairs(self.map.tiles) do
+        for i, t in pairs(row) do
+            if self.visited[j][i] then
+                local x, y = i * tile_size, j * tile_size
+                love.graphics.setColor(1, 1, 1)
+                self:draw_tile(x, y, self.map.autotiles[j][i])
+                if not self.visible[j][i] then
+                    love.graphics.setColor(0, 0, 0, 0.25)
+                    love.graphics.rectangle("fill", x, y, tile_size, tile_size)
+                end
+            end
+        end
+    end
+    love.graphics.setColor(1, 1, 1)
+    for _, trap in pairs(self.traps) do
+        trap:draw()
+    end
+    for _, enemy in pairs(self.enemies) do
+        enemy:draw()
+    end
+    for _, player in pairs(self.players) do
+        player:draw()
+    end
+    for _, projectile in pairs(self.projectiles) do
+        projectile:draw()
+    end
+    for _, animation in pairs(self.animations) do
+        animation:draw()
+    end
+    for _, popup in pairs(self.popups) do
+        popup:draw()
+    end
+end
+
+function scene:draw_tile(x, y, autotile)
+    
+end
+
+function scene:draw_minimap()
+
+end
+
+function scene:close()
+    if self.map_generator then
+        self.map_generator:release()
+        self.map_generator = nil
     end
 end
 
