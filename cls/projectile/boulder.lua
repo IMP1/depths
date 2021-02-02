9c1f6661c238d4461816e6da0b1327b5863ec436
IMP1 <huw_taylor@hotmail.co.uk>
2021-02-02 16:27:20 +0000

    Add screenshakes when moving and hitting

--- cls/projectile/boulder.lua
+++ cls/projectile/boulder.lua
@@ -1,3 +1,4 @@
+local screenshake = require 'lib.screenshake'
 local projectile = require 'cls.projectile._base'
 
 local boulder = {}
@@ -9,16 +10,22 @@ local RADIUS = 16
 local MASS = 600
 local DAMAGE = 9999
 local MIN_MASS_TO_DESTROY = 500
+local ROLL_SCREENSHAKE = 1 -- pixels
+local HIT_SCREENSHAKE_STRENGTH = 3 -- pixels
+local HIT_SCREENSHAKE_DURATION = 0.2 -- seconds
 
 function boulder.new(x, y, dx, dy)
     local self = projectile.new(x, y, dx * SPEED, dy * SPEED, RADIUS, MASS, DAMAGE)
     setmetatale(self, boulder)
 
+    -- TODO: Add and handle an animation
+
+    self.screenshake_id = screenshake.add_rumble(ROLL_SCREENSHAKE)
+
     return self
 end
 
 function boulder:hit(obj)
-    -- Don't destroy boulder on small squishy things
     if obj then
         obj:damage(max_damage)
         if obj.mass > MIN_MASS_TO_DESTROY then
@@ -27,11 +34,12 @@ function boulder:hit(obj)
     else
         self:destroy()
     end
+    screenshake.add_screenshake(HIT_SCREENSHAKE_STRENGTH, HIT_SCREENSHAKE_STRENGTH, HIT_SCREENSHAKE_DURATION)
 end
 
 function boulder:destroy()
+    screenshake.remove_rumble(self.screenshake_id)
     projectile.destroy(self)
-    -- TODO: Remove screenshake rumble
 end
 
 function boulder:update(dt, scene)
