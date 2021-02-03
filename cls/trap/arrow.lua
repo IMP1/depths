cad5949b34f3aeb02ac0e9e6e6c685d0a62ad815
IMP1 <huw_taylor@hotmail.co.uk>
2021-02-03 22:22:06 +0000

    Add delay to arrrows

--- cls/trap/arrow.lua
+++ cls/trap/arrow.lua
@@ -8,21 +8,31 @@ local arrow_trap = {}
 setmetatable(arrow_trap, trap)
 arrow_trap.__index = arrow_trap
 
+local FIRE_DELAY = 0.5 -- seconds
+
 function arrow_trap.new(trigger_x, trigger_y, arrow_x, arrow_y, ammo)
     local self = trap.new(trigger_x, trigger_y, "Arrow", 1)
     setmetatable(self, arrow_trap)
 
     self.arrow_position = vec2(arrow_x, arrow_y)
-    self.ammo = ammo or 1
+    self.ammo = ammo or 10
+    self.fire_delay = 0
 
     return self
 end
 
 function arrow_trap:trigger(scene)
-    if self.ammo == 0 then return end
+    if self.fire_delay > 0 then return end
+    if self.ammo then
+        if self.ammo <= 0 then return end
+        self.ammo = self.ammo - 1
+    end 
     self.triggered = true
-    self.ammo = self.ammo - 1
-    -- TODO: Add arrow to scene
+    local arrow_direction = (self.trigger_position - self.arrow_position):normalise()
+    local x, y = unpack(((self.arrow_position - vec2(0.5, 0.5)) * level.TILE_SIZE).data)
+    local a = arrow.new(x, y, arrow_direction.x, arrow_direction.y)
+    table.insert(scene.projectiles, a)
+    self.fire_delay = FIRE_DELAY
 end
 
 function arrow_trap:is_at_tile(i, j)
@@ -30,6 +40,13 @@ function arrow_trap:is_at_tile(i, j)
     local x2, y2 = unpack(self.arrow_position.data)
     return i >= math.min(x1, x2) and i <= math.max(x1, x2) and
            j >= math.min(y1, y2) and j <= math.max(y1, y2)
+end
+
+function arrow_trap:update(dt, ...)
+    trap.update(self, dt, ...)
+    if self.fire_delay > 0 then
+        self.fire_delay = math.max(0, self.fire_delay - dt)
+    end
 end
 
 function arrow_trap:draw(scene)
@@ -40,8 +57,6 @@ function arrow_trap:draw(scene)
         love.graphics.setColor(1, 0, 0, 0.8)
         love.graphics.rectangle("fill", x + 4, y + 4, level.TILE_SIZE - 8, level.TILE_SIZE - 8)
     end
-
-
     -- TODO: Draw a arrow shooty hole if visited
 end
 
