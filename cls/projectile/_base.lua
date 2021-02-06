0a83210a4f817b2764a7bf417b103cde0c3898b3
IMP1 <huw_taylor@hotmail.co.uk>
2021-02-06 13:25:11 +0000

    Use destroyed instead of finished; Pass self to collision check

--- cls/projectile/_base.lua
+++ cls/projectile/_base.lua
@@ -11,7 +11,7 @@ function projectile.new(x, y, vx, vy, radius, mass, damage)
 
     self.velocity = vec2(vx, vy)
     self.speed = self.velocity:magnitude()
-    self.finished = false
+    self.destroyed = false
     self.max_damage = damage
 
     return self
@@ -25,21 +25,24 @@ function projectile:hit(obj)
 end
 
 function projectile:destroy()
-    self.finished = true
+    self.destroyed = true
 end
 
 function projectile:can_move_through(old_position, new_position, scene)
-    return scene:is_pixel_passable(new_position.x, new_position.y)
+    local midpoint = (old_position + new_position) / 2
+    return scene:is_pixel_passable(new_position.x, new_position.y, self) and
+           scene:is_pixel_passable(midpoint.x, midpoint.y, self)
     -- TODO: Check along the way? For large values of dt
 end
 
 function projectile:update(dt, scene)
+    if self.destroyed then return end
     local new_pos = self.position + self.velocity * dt
     if self:can_move_through(self.position, new_pos, scene) then
         self.position = new_pos
     else
         local x, y = unpack(((new_pos + self.position) / 2).data)
-        local collider = scene:get_object_at(x, y, self.radius * self.speed * dt * 2, self)
+        local collider = scene:get_object_at(x, y, self.radius * self.speed * dt, self)
         self:hit(collider)
     end
 end
